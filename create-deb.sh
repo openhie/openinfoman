f515c66b6045c2303ef89f11ea746ba68f6b7279
Carl Leitner <litlfred@gmail.com>
2015-02-14 01:35:03 -0500

    first draft at a debain packaging to download basex, setup db, add in resources, webapp.  installed under /var/lib/openinfoman with the structure of a basex install directory.  adds /usr/local/bin/oi-start  and /usr/local/bin/oi-stop.  script create-deb.sh can be used to package and create the deb from source

---
+++ create-deb.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#Exit on error
+set -e
+#debug
+set -x
+
+PPA=mHero
+TARGET=trusty
+PKG=openinfoman
+RLS=1.0
+
+
+
+#Don't edit below
+
+
+
+HOME=`pwd`
+DEBS=$HOME/debs-${TARGET}
+echo $DEBS
+
+if [ -n "$LAUNCHPADPPALOGIN" ]; then
+  echo Using $LAUNCHPADPPALOGIN for Launchpad PPA login
+  echo "To Change You can do: export LAUNCHPADPPALOGIN=$LAUNCHPADPPALOGIN"
+else 
+  echo -n "Enter your launchpad login for the ppa and press [ENTER]: "
+  read LAUNCHPADPPALOGIN
+  echo "You can do: export LAUNCHPADPPALOGIN=$LAUNCHPADPPALOGIN to avoid this step in the future"
+fi
+
+gpg --list-public-keys | grep ^pub | awk '{print $2}' | awk -F / '{print $2}'
+
+if [ -n "$GPGKEY" ]; then
+  echo Using $GPGKEY for Launchpad PPA login
+  echo "To Change You can do: export GPGKEY=$GPGKEY"
+else 
+  echo -n "Enter your GPG key for the ppa and press [ENTER]: "
+  echo "Do: 
+     gpg --list-public-keys | grep ^pub | awk '{print \$2}' | awk -F / '{print \$2}'
+to see the available keys"
+  echo "You can do: export GPGKEY=XXXXX to avoid this step in the future"
+  exit 1
+
+fi
+
+mkdir -p $HOME/wiki && php /var/lib/mediawiki/maintenance/dumpBackup.php --current --include-files > $HOME/wiki/mediawiki-mhero-export.xml
+
+
+
+
+PKGDIR=${DEBS}/${PKG}-${RLS}~${TARGET}
+CHANGES=$DEBS/${PKG}_${RLS}~${TARGET}_source.changes
+rm -fr $PKGDIR
+mkdir -p $PKGDIR/var/lib
+cd $PKGDIR/var/lib && git clone https://github.com/openhie/$PKG openinfoman
+mkdir -p $PKGDIR/var/lib/openinfoman/repo
+mv $PKGDIR/var/lib/openinfoman/repo $PKGDIR/var/lib/openinfoman/repo-src 
+cp  -R $HOME/packaging/* $PKGDIR
+
+
+cd $PKGDIR && echo `pwd` && dpkg-buildpackage -uc -us
+#cd $PKGDIR && echo `pwd` && dpkg-buildpackage -k$GPGKEY -S -sa 
+#dput --force ppa:$LAUNCHPADPPALOGIN/$PPA  $CHANGES
+
+
