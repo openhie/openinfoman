1652c00c058dd95d89a731867038154f9e3d4c5b
Carl Leitner <litlfred@gmail.com>
2015-02-15 08:43:44 -0500

    moving towards supporting multiple Ubuntu releases

---
+++ packaging/create-deb.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#Exit on error
+set -e
+#debug
+set -x
+
+
+PPA=mhero
+
+#Don't edit below
+
+HOME=`pwd`
+
+cd targets
+TARGETS=(*)
+echo $TARGETS
+cd $HOME
+
+
+
+if [ -n "$LAUNCHPADPPALOGIN" ]; then
+  echo Using $LAUNCHPADPPALOGIN for Launchpad PPA login
+  echo "To Change You can do: export LAUNCHPADPPALOGIN=$LAUNCHPADPPALOGIN"
+else 
+  echo -n "Enter your launchpad login for the ppa and press [ENTER]: "
+  read LAUNCHPADPPALOGIN
+  echo "You can do: export LAUNCHPADPPALOGIN=$LAUNCHPADPPALOGIN to avoid this step in the future"
+fi
+
+KEY=
+
+if [ -n "${DEB_SIGN_KEYID}" ]; then
+  echo Using ${DEB_SIGN_KEYID} for Launchpad PPA login
+  echo "To Change You can do: export DEB_SIGN_KEYID=${DEB_SIGN_KEYID}"
+  echo "For unsigned you can do: export DEB_SIGN_KEYID="
+  KEY="-k${DEB_SIGN_KEYID}"
+else 
+  echo "No DEB_SIGN_KEYID key has been set.  Will create an unsigned"
+  echo "To set a key for signing do: export DEB_SIGN_KEYID=<KEYID>"
+  echo "Use gpg --list-keys to see the available keys"
+  KEY="-uc -us"
+fi
+
+
+BUILD=$HOME/builds
+AWK=/usr/bin/awk
+HEAD=/usr/bin/head
+
+for TARGET in "${TARGETS[@]}"
+do
+    TARGETDIR=$HOME/targets/$TARGET
+    echo "$HEAD -1 $TARGETDIR/debian/changelog | $AWK '{print $2}' | $AWK -F~ '{print $1}' | $AWK -F\( '{print $2}'"
+    RLS=`$HEAD -1 $TARGETDIR/debian/changelog | $AWK '{print $2}' | $AWK -F~ '{print $1}' | $AWK -F\( '{print $2}'`
+    PKG=`$HEAD -1 $TARGETDIR/debian/changelog | $AWK '{print $1}'`
+    PKGDIR=${BUILD}/${PKG}-${RLS}~${TARGET}
+    CHANGES=${BUILD}/${PKG}_${RLS}~${TARGET}_source.changes
+
+    echo  "echo Building Package $PKG  on Release $RLS for Target $TARGET"
+
+    rm -fr $PKGDIR
+    mkdir -p $PKGDIR/var/lib
+    cd $PKGDIR/var/lib && git clone https://github.com/openhie/$PKG openinfoman
+    rm -fr $PKGDIR/var/lib/openinfoman/.git
+    mkdir -p $PKGDIR/var/lib/openinfoman/repo
+    mv $PKGDIR/var/lib/openinfoman/repo $PKGDIR/var/lib/openinfoman/repo-src 
+    cp  -R $TARGETDIR/* $PKGDIR
+
+    cd $PKGDIR  
+    #CMD="dpkg-buildpackage $KEY  -S -sa "
+    DPKGCMD="dpkg-buildpackage $KEY  -S -sa "
+    $DPKGCMD
+
+
+    cd ~/
+    echo `pwd`
+    if [[ -n "${DEB_SIGN_KEYID}" && -n "{$LAUNCHPADLOGIN}" ]]; then
+	DPUTCMD="dput --force ppa:$LAUNCHPADPPALOGIN/$PPA  $CHANGES"
+	$DPUTCMD
+        #DPUTCMD="dput -u --force ppa:$LAUNCHPADPPALOGIN/$PPA  $CHANGES"
+        #cd $PKGDIR && dpkg-buildpackage -uc -us
+        #cd $PKGDIR && dpkg-buildpackage -k${DEB_SIGN_KEYID} -S -sa 
+        #
+    else
+	echo "Not uploaded to launchpad"
+    fi
+done
+
+
+cd $HOME
+
